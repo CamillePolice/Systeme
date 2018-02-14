6439be18d2ddc9fe586352ad940540eaeaf47cb1
Camille Police <camille.police@icloud.com>
2018-02-14 09:27:34 +0100

    Ajout du read()

--- Projet/src/lecture.c
+++ Projet/src/lecture.c
@@ -1,26 +1,32 @@
-#include "audio.h"
+#include "include\audio.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include  <fcntl.h>
 
 int main(int argc, char **argv) {
   // Variables
-  int sample_rate, sample_size, channels;
+  int sample_rate, sample_size, channels, audio_fd, nbBytes;
   char *pathTofileName;
   char *extension = ".wav";
+  char buffer[50];
+
+  nbBytes = sizeOf(buffer);
+
 
   // Streaming of each files passed in parameters
   for(int i=0;i< argc;i++) {
-    printf("Streaming of the following file -> %s", argc[i]);
+    printf("Streaming of the following file -> %s", argv[i]);
 
     // Opening the WAV-file for reading
     if(aud_readinit(pathTofileName, &sample_rate, &sample_size, &channels) < 0)
-      printf("Can't open the following file -> %s", argc[i]);
+      printf("Can't open the following file -> %s", argv[i]);
 
     // Stream the music on the speaker
     if(audio_fd = aud_writeinit(sample_rate, sample_size, channels) < 0)
       printf("Can't send it to the speaker");
 
+    read(audio_fd, buffer, nbBytes);
     close(audio_fd);
   }
 
