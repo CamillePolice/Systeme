60b9ba94ae511413e0e9562f78bc7dce8ef81add
Camille Police <camille.police@icloud.com>
2018-02-14 09:43:56 +0100

    audio_read+audio_write

--- Projet/src/lecture.c
+++ Projet/src/lecture.c
@@ -1,17 +1,19 @@
 #include "include\audio.h"
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include  <fcntl.h>
+
 
 int main(int argc, char **argv) {
   // Variables
-  int sample_rate, sample_size, channels, audio_fd, nbBytes;
+  int sample_rate, sample_size, channels, audio_read, audio_write, nb_bytes;
   char *pathTofileName;
   char *extension = ".wav";
   char buffer[50];
 
-  nbBytes = sizeOf(buffer);
+  nbBytes = sizeof(buffer);
 
 
   // Streaming of each files passed in parameters
@@ -19,14 +21,16 @@ int main(int argc, char **argv) {
     printf("Streaming of the following file -> %s", argv[i]);
 
     // Opening the WAV-file for reading
-    if(aud_readinit(pathTofileName, &sample_rate, &sample_size, &channels) < 0)
+    if(audio_read = aud_readinit(pathTofileName, &sample_rate, &sample_size, &channels) < 0)
       printf("Can't open the following file -> %s", argv[i]);
 
+    read(audio_fd, buffer, nb_bytes);
+
     // Stream the music on the speaker
-    if(audio_fd = aud_writeinit(sample_rate, sample_size, channels) < 0)
+    if(audio_write = aud_writeinit(sample_rate, sample_size, channels) < 0)
       printf("Can't send it to the speaker");
 
-    read(audio_fd, buffer, nbBytes);
+    write(audio_write, buffer, nb_bytes)
     close(audio_fd);
   }
 
