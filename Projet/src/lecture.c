8689c3f81fdf9a36b624e885c6ad8203c9b66f80
CamillePolice <camillepolice@icloud.com>
2018-03-14 09:06:47 +0100

    Bla

--- Projet/src/lecture.c
+++ Projet/src/lecture.c
@@ -1,4 +1,4 @@
-#include "include\audio.h"
+#include "audio.h"
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -13,25 +13,27 @@ int main(int argc, char **argv) {
   char *extension = ".wav";
   char buffer[50];
 
-  nbBytes = sizeof(buffer);
+  nb_bytes = sizeof(buffer);
 
 
   // Streaming of each files passed in parameters
   for(int i=0;i< argc;i++) {
     printf("Streaming of the following file -> %s", argv[i]);
-
+    strcat(pathTofileName, argv[i]);
+    strcat(pathTofileName, exetension);
     // Opening the WAV-file for reading
     if(audio_read = aud_readinit(pathTofileName, &sample_rate, &sample_size, &channels) < 0)
       printf("Can't open the following file -> %s", argv[i]);
 
-    read(audio_fd, buffer, nb_bytes);
+    read(audio_read, buffer, nb_bytes);
 
     // Stream the music on the speaker
     if(audio_write = aud_writeinit(sample_rate, sample_size, channels) < 0)
       printf("Can't send it to the speaker");
 
-    write(audio_write, buffer, nb_bytes)
-    close(audio_fd);
+    write(audio_write, buffer, nb_bytes);
+
+    close(audio_write);
   }
 
   // All fine
